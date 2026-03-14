78fc07b6769ed6abda413233bf07f5ed15807767
Jakov <23292936+itsJakov@users.noreply.github.com>
2026-03-14 21:24:03 +0100

    Generic convert function

--- zson/src/coder.rs
+++ zson/src/coder.rs
@@ -5,4 +5,9 @@ pub trait Coder {
 
     fn encode<T: Encodable>(value: &T) -> <Self::Type as ToOwned>::Owned;
     fn decode<T: Decodable>(value: &Self::Type) -> Option<T>;
+
+    fn convert<Dst: Coder>(input: &Self::Type) -> Option<<Dst::Type as ToOwned>::Owned> {
+        let value = Self::decode::<Value>(input)?;
+        Some(Dst::encode::<Value>(&value))
+    }
 }
